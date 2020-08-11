fa39ef0a7a555f71a7de80dee5bc7f72179bdfb6
lisau <lisau@DESKTOP-B074GT4>
2020-08-11 09:39:44 +0200

    First Commit

---
+++ MicroService/src/test/java/com/LISULA/MicroService/AppTest.java
@@ -0,0 +1,20 @@
+package com.LISULA.MicroService;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+
